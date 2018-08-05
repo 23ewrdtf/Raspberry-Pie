52fde58f5eddc0c191c53cc3912c096ff82bb1f6
tretos53 <adam.g@outlook.com>
2018-08-05 10:46:18 +0100

    Update rPi3-ap-setup.sh

--- rPi3-ap-setup.sh
+++ rPi3-ap-setup.sh
@@ -1,9 +1,7 @@
-#From https://gist.github.com/Lewiscowles1986/fecd4de0b45b2029c390
-
 #!/bin/bash
 #
 # This version uses September 2017 august stretch image, please use this image
-#
+# From https://gist.github.com/Lewiscowles1986/fecd4de0b45b2029c390
 
 if [ "$EUID" -ne 0 ]
 	then echo "Must be root"
@@ -32,6 +30,7 @@ apt-get install hostapd dnsmasq -yqq
 cat > /etc/dnsmasq.conf <<EOF
 interface=wlan0
 dhcp-range=10.0.0.2,10.0.0.5,255.255.255.0,12h
+address=/#/10.0.0.1
 EOF
 
 cat > /etc/hostapd/hostapd.conf <<EOF
@@ -39,11 +38,6 @@ interface=wlan0
 hw_mode=g
 channel=10
 auth_algs=1
-wpa=2
-wpa_key_mgmt=WPA-PSK
-wpa_pairwise=CCMP
-rsn_pairwise=CCMP
-wpa_passphrase=$APPASS
 ssid=$APSSID
 ieee80211n=1
 wmm_enabled=1
@@ -63,7 +57,9 @@ iface wlan0 inet static
 	netmask 255.255.255.0
 	network 10.0.0.0
 	broadcast 10.0.0.255
-
+auto eth0
+	allow-hotplug eth0
+	iface eth0 inet dhcp
 EOF
 
 echo "denyinterfaces wlan0" >> /etc/dhcpcd.conf
@@ -73,5 +69,8 @@ systemctl enable dnsmasq
 
 sudo service hostapd start
 sudo service dnsmasq start
+
+sudo update-rc.d dnsmasq defaults
+sudo update-rc.d hostapd defaults
 
 echo "All done! Please reboot"
